ee901db398427791888e3792bfabbf6524ef301f
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-10 18:00:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,3 +37,14 @@ def valid_move? (board, index)
   end
 end
 
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, character = "X")
+    display_board(board)
+  else
+    turn(board)
+  end
+end
