05f312c2b00d3e0e5c80392b330a8a1dd7b235c2
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-10 18:03:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,3 +67,40 @@ def current_player(board)
   end
 return current_player
 end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |wincombo|
+  if board[wincombo[0]] == "X" && board[wincombo[1]] == "X" && board[wincombo[2]] == "X"
+    return wincombo
+  elsif board[wincombo[0]] == "O" && board[wincombo[1]] == "O" && board[wincombo[2]] == "O"
+    return wincombo
+  end
+end
+false
+end
+
+def full?(board)
+  board.all? {|i| i == "X" || i == "O"}
+end
+
+def draw?(board)
+    if !won?(board) && full?(board)
+      return true
+    else
+      return false
+    end
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
+end
