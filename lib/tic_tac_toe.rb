d5d933c24349a6efb99338a422192437542f0817
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-10 18:01:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,3 +58,12 @@ def turn_count(board)
   end
 return counter
 end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    current_player = "X"
+  else
+    current_player = "O"
+  end
+return current_player
+end
